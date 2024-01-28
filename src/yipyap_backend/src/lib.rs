84b3aef896fb8d8a47233e5dbc5286aff4e7506d
amschel99 <6290.2020@students.ku.ac.ke>
2024-01-28 16:30:22 +0300

    Unit test for get_question

--- src/yipyap_backend/src/lib.rs
+++ src/yipyap_backend/src/lib.rs
@@ -1,39 +1,50 @@
 use std::cell::RefCell;
 
-use ic_cdk::{query,update};
-use ic_stable_structures::{BTreeMap, memory_manager::{MemoryId, MemoryManager, VirtualMemory},DefaultMemoryImpl};
+use ic_cdk::{query, update};
+use ic_stable_structures::{
+    memory_manager::{MemoryId, MemoryManager, VirtualMemory},
+    BTreeMap, DefaultMemoryImpl,
+};
 
 type Memory = VirtualMemory<DefaultMemoryImpl>;
 
-type VOTE=(String, u32);
-
+type VOTE = (String, u32);
 
 thread_local! {
-   
+
     static MEMORY_MANAGER: RefCell<MemoryManager<DefaultMemoryImpl>> =
         RefCell::new(MemoryManager::init(DefaultMemoryImpl::default()));
 
-   
+
     static MAP: RefCell<BTreeMap<String, u32, Memory>> = RefCell::new(
         BTreeMap::init(
             MEMORY_MANAGER.with(|m| m.borrow().get(MemoryId::new(0))),
         )
     );
+    static QUESTION:RefCell<String>=RefCell::new(String::from("What is your favorite programming languange?"));
+
+
 }
 
 #[query]
-fn get_question()->String {
-
-  String::from("What is your favorite Programming languange")
-
+fn get_question() -> String {
+    let res = QUESTION.with(|q| q.borrow().to_string());
+    res
 }
 
 #[query]
 
 fn get_votes() -> Option<Vec<VOTE>> {
-    let borrowed_value:Vec<_>=MAP.with(|map|map.borrow().iter().collect());
+    let borrowed_value: Vec<_> = MAP.with(|map| map.borrow().iter().collect());
     Some(borrowed_value)
-  
- 
 }
 
+#[update]
+fn vote() {}
+
+#[test]
+fn works() {
+    let res = QUESTION.with(|q| q.borrow().to_string());
+
+    assert_eq!("What is your favorite programming languange?", dbg!(res));
+}
