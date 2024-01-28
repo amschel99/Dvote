72bf55b139f071c799173f1e0cc49e7df84dd44a
amschel99 <6290.2020@students.ku.ac.ke>
2024-01-28 17:37:54 +0300

    add vote update method and write unit test for it

--- src/yipyap_backend/src/lib.rs
+++ src/yipyap_backend/src/lib.rs
@@ -1,4 +1,4 @@
-use std::cell::RefCell;
+use std::{borrow::BorrowMut, cell::RefCell};
 
 use ic_cdk::{query, update};
 use ic_stable_structures::{
@@ -40,11 +40,32 @@ fn get_votes() -> Option<Vec<VOTE>> {
 }
 
 #[update]
-fn vote() {}
+fn vote(entry: String) {
+    let borrowed_value: Vec<_> = MAP.with(|map| map.borrow().iter().collect());
+    let matching_val: Vec<_> = borrowed_value.iter().filter(|val| val.0 == entry).collect();
+    if matching_val.len() == 0 {
+        MAP.with(|m| m.borrow_mut().insert(entry, 1));
+        //there is no such key so create it and add 1 vote
+    }
+}
 
 #[test]
-fn works() {
+fn question_test() {
     let res = QUESTION.with(|q| q.borrow().to_string());
 
     assert_eq!("What is your favorite programming languange?", dbg!(res));
+}
+#[test]
+fn vote_for_new_languange_works() {
+    let borrowed_value: Vec<_> = MAP.with(|map| map.borrow().iter().collect());
+    let matching_val: Vec<_> = borrowed_value
+        .iter()
+        .filter(|val| val.0 == "Rust".to_string())
+        .collect();
+    if matching_val.len() == 0 {
+        MAP.with(|m| m.borrow_mut().insert("Rust".to_string(), 1));
+        let new_votes: Vec<_> = MAP.with(|map| map.borrow().iter().collect());
+
+        assert_eq!(new_votes.get(0), Some(&("Rust".to_string(), 1)));
+    }
 }
